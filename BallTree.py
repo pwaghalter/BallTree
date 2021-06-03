897dff6e5166b876d927f02c4b8bd82f3828e104
Penina Waghalter <77010726+pwaghalter@users.noreply.github.com>
2021-06-02 21:01:08 -0700

    Update BallTree.py

--- BallTree.py
+++ BallTree.py
@@ -1,7 +1,3 @@
-#"I hereby certify that this program is solely the result of my own work and is
-#in compliance with the Academic Integrity policy of the course syllabus and the 
-#academic integrity policy of the CS department.”
-
 import math
 import random as r
 import statistics as s
